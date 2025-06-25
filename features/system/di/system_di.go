1eb0990359b469c5f41db94aeb39cee2cc3fa8f0
D'Bonis <dev@rodolfodebonis.com.br>
2025-06-25 06:40:08 -0300

    feat(system): update dependency injection wiring

--- features/system/di/system_di.go
+++ features/system/di/system_di.go
@@ -1,1 +1,11 @@
 package di
+
+import (
+	"github.com/RodolfoBonis/microdetect-api/features/system/data/services"
+	"github.com/RodolfoBonis/microdetect-api/features/system/domain/usecases"
+)
+
+func SystemInjection() usecases.SystemUseCase {
+	service := services.NewSystemService()
+	return usecases.NewSystemUseCase(service)
+}
