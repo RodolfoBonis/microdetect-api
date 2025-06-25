7f7b308d6d1b90a9544bf4b0db6a26825db675fe
D'Bonis <dev@rodolfodebonis.com.br>
2025-06-25 09:37:13 -0300

    chore: update dependencies and router for new features and refactors

--- go.mod
+++ go.mod
@@ -70,6 +70,8 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.uber.org/dig v1.19.0 // indirect
+	go.uber.org/fx v1.24.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.7.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
--- routes/router.go
+++ routes/router.go
@@ -2,22 +2,33 @@ package routes
 
 import (
 	"github.com/RodolfoBonis/microdetect-api/core/health"
+	"github.com/RodolfoBonis/microdetect-api/core/logger"
+	"github.com/RodolfoBonis/microdetect-api/core/middlewares"
 	"github.com/RodolfoBonis/microdetect-api/features/auth"
+	auth_uc "github.com/RodolfoBonis/microdetect-api/features/auth/domain/usecases"
 	"github.com/RodolfoBonis/microdetect-api/features/system"
+	system_uc "github.com/RodolfoBonis/microdetect-api/features/system/domain/usecases"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func InitializeRoutes(router *gin.Engine) {
+func InitializeRoutes(
+	router *gin.Engine,
+	systemUc system_uc.SystemUseCase,
+	authUc auth_uc.AuthUseCase,
+	monitoring *middlewares.MonitoringMiddleware,
+	protectFactory func(handler gin.HandlerFunc, role string) gin.HandlerFunc,
+	logger logger.Logger,
+) {
 
 	root := router.Group("/v1")
 
 	root.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
-	health.InjectRoute(root)
-	auth.InjectRoutes(root)
-	system.InjectRoutes(root)
+	health.HealthRoutes(root, logger)
+	auth.AuthRoutes(root, authUc, protectFactory)
+	system.SystemRoutes(root, systemUc)
 }
