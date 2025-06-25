1bafda6fc7840bd62d28f58d8b2a7369b2c36bbc
D'Bonis <dev@rodolfodebonis.com.br>
2025-06-25 06:40:17 -0300

    feat(system): implement system endpoints handlers

--- features/system/system_route.go
+++ features/system/system_route.go
@@ -1,1 +1,17 @@
 package system
+
+import (
+	"github.com/RodolfoBonis/microdetect-api/features/system/di"
+	"github.com/gin-gonic/gin"
+)
+
+func InjectRoutes(route *gin.RouterGroup) {
+	var systemUc = di.SystemInjection()
+
+	systemRoute := route.Group("/system")
+	systemRoute.GET("/storage", systemUc.GetStorage)
+	systemRoute.GET("/memory", systemUc.GetMemory)
+	systemRoute.GET("/status", systemUc.GetSystemStatus)
+	systemRoute.GET("/cpu", systemUc.GetCPU)
+
+}
